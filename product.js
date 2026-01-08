4c91899050637fedf8e95e17734cc022fa3e446f
vidhi-maniar-png <vidhimaniar004@gmail.com>
2026-01-08 09:33:39 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,67 @@
+// product.js
+const products = [
+  {
+    name: "Bearing",
+    image: "images/bearing.jpg",
+    price: 150,
+    oldPrice: 150,
+    discount: 10,
+    // link: "product_purchase.html"
+  },
+  {
+    name: "Capacitors",
+    image: "images/bearing.jpg",
+    price: 150,
+    oldPrice: 150,
+    discount: 10,
+    link: "#"
+  },
+  {
+    name: "Wire",
+    image: "images/gift.png",
+    price: 150,
+    oldPrice: 150,
+    discount: 10,
+    link: "#"
+  },
+  {
+    name: "Fans",
+    image: "images/gift.png",
+    price: 150,
+    oldPrice: 150,
+    discount: 10,
+    link: "#"
+  },
+  {
+    name: "Fan Cover",
+    image: "images/gift.png",
+    price: 150,
+    oldPrice: 150,
+    discount: 10,
+    link: "#"
+  },
+  {
+    name: "Fuse",
+    image: "images/gift.png",
+    price: 150,
+    oldPrice: 150,
+    discount: 10,
+    link: "#"
+  },
+  {
+    name: "Type A Bearing",
+    image: "images/gift.png",
+    price: 150,
+    oldPrice: 150,
+    discount: 10,
+    link: "#"
+  },
+  {
+    name: "Table Fan",
+    image: "images/gift.png",
+    price: 150,
+    oldPrice: 150,
+    discount: 10,
+    link: "#"
+  }
+];
